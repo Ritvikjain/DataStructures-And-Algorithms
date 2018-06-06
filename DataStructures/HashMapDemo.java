3bd1902169a71d0c81e666dbe28d3a24dda04d43
Ritvikjain <ritvikjain2@gmail.com>
2018-06-06 23:43:24 +0530

    HashMap

---
+++ DataStructures/HashMapDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class HashMapDemo{
+	public static void main(String args[]){
+		HashMap<String,Integer> hm=new HashMap();
+		hm.put("a",new Integer(100));
+		hm.put("b",new Integer(200));
+		hm.put("c",new Integer(300));
+		hm.put("d",new Integer(400));
+		hm.put("e",new Integer(500));
+		Set<Map.Entry<String,Integer>> st=hm.entrySet();
+		for(Map.Entry<String,Integer> me:st)
+		{
+			System.out.println(me.getKey()+":");
+			System.out.println(me.getValue());
+		}
+	}
+}
