db6a85d71f2a8d94b02e6c48c5467be22af3ab30
Ritvikjain <ritvikjain2@gmail.com>
2018-07-13 01:41:05 +0530

    circular linked list

--- DataStructures/LinkedList/CircularLinkedList.cpp
+++ DataStructures/LinkedList/CircularLinkedList.cpp
@@ -1,5 +1,39 @@
 #include<iostream>
 using namespace std;
-int main(){
-    
+struct Node{
+    int data;
+    struct Node *next;
+}
+class List{
+    public: Node *head,*tail;
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void createNode(int value){
+        Node *temp = new Node;
+        temp->data = value;
+        temp->next = NULL;
+        if(head == NULL)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else{
+            tail->next = temp;
+            tail = temp;
+            tail->next = head;
+        }
+    }
+    void printList()
+    {
+        Node *temp = head;
+        if(head != NULL){
+            do{
+                cout<<temp->data;
+                temp temp = temp->next;
+            }while(temp != head);
+        }
+    }
 }
