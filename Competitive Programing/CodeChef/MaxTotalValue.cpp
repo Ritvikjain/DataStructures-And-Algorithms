52fe838c8b79fdc5f600300616f1ab8a3d3e35ac
Ritvikjain <ritvikjain2@gmail.com>
2019-04-20 23:07:56 +0530

    CodeChef Problems

---
+++ Competitive Programing/CodeChef/MaxTotalValue.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool cmp(const pair<int,int> &a,const pair<int,int> &b)
+{
+	if(a.second!=b.second)
+	{
+		return a.second > b.second;
+	}
+	return a.first < b.first;
+}
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	pair<int,int>* pieces = new pair<int,int>[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		cin>>pieces->first>>pieces->second;
+	}
+	
+	sort(pieces,pieces+n,cmp);
+	multiset<int> m;
+	for(int i=0;i<k;i++)
+	{
+		int wieght;
+		cin>>wieght;
+		m.insert(wieght);
+	}
+	long ans;
+	for(int i=0;i<n;i++)
+	{
+		if(m.empty())
+		{
+			break;
+		}
+		else if(m.lower_bound(pieces[i].first) != m.end()){
+			ans += pieces[i].second;
+			m.erase(m.lower_bound(pieces[i].first));
+		}
+	}
+	cout<<ans;
+	return 0;
+}
