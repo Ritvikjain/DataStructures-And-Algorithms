fa6940dac84818f490fbfbba7fd797c709c716a0
Ritvikjain <ritvikjain2@gmail.com>
2019-05-09 15:38:59 +0530

    lcs iterative solution

--- Algorithms/Dynamic Programming/LongestCommonSubsequence.cpp
+++ Algorithms/Dynamic Programming/LongestCommonSubsequence.cpp
@@ -1,5 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+int lcsIterative(char* s1,char* s2)
+{
+	int m = strlen(s1);
+	int n = strlen(s2);
+	int **dp = new int*[m+1];
+	for(int i=0;i<=m;i++)
+	{
+		dp[i] = new int[n+1];
+	}
+	for(int i=0;i<m;i++)
+		dp[i][0] = 0;
+	for(int i=0;i<n;i++)
+	    dp[0][i] = 0;
+	for(int i=1;i<=m;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			if(s1[m-i] == s2[n-j])
+			{
+				dp[i][j] = 1 + dp[i-1][j-1];
+			}
+			else{
+				dp[i][j] = max( dp[i-1][j], dp[i][j-1]);
+			}
+		}
+	}
+	int ans = dp[m][n];
+	for(int i=0;i<=m;i++)
+		delete [] dp[i];
+	delete [] dp;
+	return ans;
+}
 int lcsHelper(char *s1,char *s2,int m,int n,int ** dp){
 	if(m == 0 || n == 0)
 		return 0;
@@ -38,6 +70,6 @@ int main()
 {
 	char s1[100],s2[100];
 	cin >> s1 >> s2;
-	cout << lcs(s1,s2);
+	cout << lcsIterative(s1,s2);
 	return 0;
 }
