0032552db56fb1fef912291d1718448e35533917
Ritvikjain <ritvikjain2@gmail.com>
2019-06-03 00:43:16 +0530

    Graph DFS Traversal

---
+++ DataStructures/Graphs/DFS.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printDFS(int ** edges,int n,int sv,bool * visited)
+{
+	cout << sv << endl;
+	visited[sv] = true;
+	for(int i=0;i<n;i++)
+	{
+		if(i == sv)
+			continue;
+		if(edges[sv][i] == 1 && !visited[i]){
+			printDFS(edges,n,i,visited);
+		}
+	}
+}
+int main()
+{
+	int n,e;
+	cin >> n >> e;
+	int **edges = new int*[n];
+	for(int i=0;i<n;i++)
+	{
+		edges[i] = new int[n];
+		for(int j=0;j<n;j++)
+			edges[i][j] = 0;
+	}
+	for(int i=0;i<e;i++)
+	{
+		int x,y;
+		cin >> x >> y;
+		edges[x][y] = 1;
+		edges[y][x] = 1;
+	}
+	bool * visited = new bool[n];
+	for(int i=0;i<n;i++)
+		visited[i] = false;
+	printDFS(edges,n,0,visited);
+	return 0;
+}
